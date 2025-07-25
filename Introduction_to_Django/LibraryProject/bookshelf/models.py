112dce8f7dee4fe860b946ba4c51657c58953d7a
ben-ans <benansong536@gmail.com>
2025-07-25 04:18:43 +0100

    Displaying the data in my admin page

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -5,3 +5,8 @@ class Books(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+
+    #CREATE A STRING FUNCTION TO DISPLAY THE DATA
+    def __str__(self):
+        return f"{self.title} | Author: {self.author} | Publication year: {self.publication_year}"
